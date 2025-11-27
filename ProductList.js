0c37b0aab9fc7b6227d3a21ed2d8746e06f93a8c
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2025-11-27 19:10:18 +0530

    Refactor ProductList component and add Product details

--- ProductList.js
+++ ProductList.js
@@ -1,12 +1,35 @@
-import Product from './Product';
-export default function ProductList(props) 
-{ 
-    
+export default function Product(props) {
+  console.log(props);
+
 
   return (
-    props.productList.map((product,i) => {
-        return <Product product = {product} key={i}/>
-    })
-
- )
+    <div className="row">
+      <div className="col-5">
+        <h2>
+          {props.product.name}{" "}
+          <span class="badge text-bg-secondary">₹{props.product.price}</span>
+        </h2>
+      </div>
+      <div className="col-3">
+        <div
+          class="btn-group"
+          role="group"
+          aria-label="Basic mixed styles example"
+      >
+          <button type="button" class="btn btn-danger">
+           -
+          </button>
+          <button type="button" class="btn btn-warning">
+           {props.product.quantity}
+          </button>
+          <button type="button" class="btn btn-success" onClick={ () => props.incrementQuantity(props.index)}>
+           +
+          </button>
+        </div>
+      </div>
+      <div className="col-4">
+        {props.product.quantity * props.product.price}
+      </div>
+    </div>
+  );
 }
