3cae7d0c6c313ac4cd154227262991d23b4912e8
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2025-11-28 23:38:33 +0530

    Enhance ProductList with conditional rendering
    Added conditional rendering for empty product list and included decrement and remove item functionalities.

--- ProductList.js
+++ ProductList.js
@@ -1,13 +1,18 @@
 import Product from "./Product";
 export default function ProductList(props) {
-  return props.productList.map((product, i) => {
+ 
     return (
-      <Product
+      props.productList.length > 0 ? (
+      props.productList.map((product, i) => {
+       return <Product
         product={product}
         key={i}
         incrementQuantity={props.incrementQuantity}
+        decrementQuantity ={props.decrementQuantity}
         index={i}
+        removeItem={props.removeItem}
       />
-    );
-  });
+      })): <h1>No Products Exists in the card</h1>
+  )
+
 }
