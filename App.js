2f345c5fb4c1ea068326f07a21d8983a199db837
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2025-11-28 23:36:29 +0530

    Refactor App component to manage cart functionality

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./componets/Navbar";
+import Footer from "./componets/Footer";
 import ProductList from "./componets/ProductList";
+import AddItem from "./componets/AddItem";
 
 function App() {
   const productListData = [
     {
       price: 99999,
       name: "iphone 14 pro max",
-      quantity: 1,
+      quantity: 0,
     },
     {
       price: 9999,
@@ -17,20 +19,72 @@ function App() {
     },
   ];
 
-//Hook 
-  let [productList, setproductList] = useState(productListData)
+  //Hook
+  let [productList, setproductList] = useState(productListData);
+  let [totalAmount, setTotalAmount] = useState(0);
 
-const incrementQuantity = (index) =>{
-  let newproductList = [...productList]
-  newproductList[index].quantity ++
-  setproductList(newproductList);
-}
- return (
+  const incrementQuantity = (index) => {
+    let newproductList = [...productList];
+    let newTotalAmount = totalAmount;
+    newproductList[index].quantity++;
+  totalAmount += newproductList[index].price;
+    setproductList(newproductList);
+    setTotalAmount(totalAmount);
+  };
+
+  const decrementQuantity = (index) => {
+    let newproductList = [...productList];
+    let newTotalAmount = totalAmount;
+    if (newproductList[index].quantity > 0) {
+      newproductList[index].quantity--;
+      newTotalAmount -= newproductList[index].price;
+    }
+    setproductList(newproductList);
+    setTotalAmount(newTotalAmount);
+  };
+
+  const resetCart = () => {
+    let newproductList = [...productList];
+    newproductList.map((products) => {
+      products.quantity = 0;
+    });
+    setproductList(newproductList);
+    setTotalAmount(0);
+  };
+
+  const removeItem = (index) => {
+    let newproductList = [...productList];
+    let newTotalAmount = totalAmount;
+    newTotalAmount -=
+      newproductList[index].quantity * newproductList[index].price;
+    setTotalAmount(newTotalAmount);
+    newproductList.splice(index, 1);
+    setproductList(newproductList);
+  };
+  
+  const addItem = (name, price) => {
+    let newproductList = [...productList];
+    newproductList.push({
+      price: price,
+      name: name,
+      quantity: 0,
+    });
+    setproductList(newproductList);  
+  };
+
+  return (
     <>
       <Navbar />
       <main className="container mt-4">
-      <ProductList productList={productList} incrementQuantity= {incrementQuantity} />
+        <AddItem  addItem={addItem}/>
+        <ProductList
+          productList={productList}
+          incrementQuantity={incrementQuantity}
+          decrementQuantity={decrementQuantity}
+          removeItem={removeItem}
+        />
       </main>
+      <Footer totalAmount={totalAmount} resetCart={resetCart} />
     </>
   );
 }
