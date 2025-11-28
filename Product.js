bca41b8ef3840ebb28d8ec230343f5438f07fe10
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2025-11-28 23:38:57 +0530

    Refactor Product component with quantity decrement and remove

--- Product.js
+++ Product.js
@@ -1,35 +1,36 @@
 export default function Product(props) {
-  console.log(props);
+ 
 
 
   return (
-    <div className="row">
+    <div className="row mt-3">
       <div className="col-5">
         <h2>
           {props.product.name}{" "}
-          <span class="badge text-bg-secondary">₹{props.product.price}</span>
+          <span  className="badge text-bg-secondary">₹{props.product.price}</span>
         </h2>
       </div>
       <div className="col-3">
         <div
-          class="btn-group"
+          className="btn-group"
           role="group"
           aria-label="Basic mixed styles example"
       >
-          <button type="button" class="btn btn-danger">
+          <button type="button" className="btn btn-danger" onClick={()=> props.decrementQuantity(props.index)}>
            -
           </button>
-          <button type="button" class="btn btn-warning">
+          <button type="button" className="btn btn-warning">
            {props.product.quantity}
           </button>
-          <button type="button" class="btn btn-success" onClick={ () => props.incrementQuantity(props.index)}>
+          <button type="button" className="btn btn-success" onClick={ () => props.incrementQuantity(props.index)}>
            +
           </button>
         </div>
       </div>
-      <div className="col-4">
+      <div className="col-2">
         {props.product.quantity * props.product.price}
       </div>
+      <button className="col-2 btn btn-danger" onClick={ () => props.removeItem(props.index)}>Remove</button>
     </div>
   );
 }
